68b3420067e9e131ece03f334fa2bc44b9acc0ae
Rauan Meirelles <rauanzin95@gmail.com>
2021-01-20 17:46:45 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///            Projeto Sistema de Controle de Estacionamentos               ///
+///                Developed by  @flgorgonio - Jan, 2021                    ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void menuSobre(void);
+
+int main(void) {
+    menuSobre();
+    return 0;
+}
+
+
+void menuSobre(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///            Projeto Sistema de Controle de Estacionamentos               ///\n");
+    printf("///              = == ==       PARKINGMOD        = == ==                    ///\n");
+    printf("///                Developed by  @flgorgonio - Jan, 2021                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///  = == == ParkingMod: Um Sistema de Controle de Estacionamentos == == =  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///    Programa utilizado com a intenção de facilitar a admnistração de     ///\n");
+    printf("///   estacionamentos, para fins de organização e de admnistração. O        ///\n");
+    printf("///   programa contém os principais módulos e funcionalidades úteis que     ///\n");
+    printf("///   serão usados para tornar o estacionamneto mais viável e mais bem      ///\n");
+    printf("///   avaliado por seus ultilizadores. Como: Vagas disponíveis e            ///\n");
+    printf("///   Indisponíveis, Vagas para cada tipo de Veículo, Vagas Premium,        ///\n");
+    printf("///   Preços por hora de cada tipo de vaga, entre outros, como o Lucro      ///\n");
+    printf("///   final diário, Avaliação do cliente e Sugestões de melhoras.           ///\n");
+    printf("///                                                                         ///\n");
+    printf("///      Em desenvolvimento pelo aluno Rauan Meirelles Dantas de Araújo     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
