384d8c125028f8780f1b255583386011c88e25d1
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2021-01-28 14:57:22 -0300

    Add MenuFuncionario

--- ParkingMod.c
+++ ParkingMod.c
@@ -16,6 +16,7 @@
 void menuPrincipal(void);
 void telaSobre(void);
 void menuCliente(void);
+void menuFuncionario(void);
 
 
 ///
@@ -24,6 +25,7 @@ void menuCliente(void);
 int main(void) {
     menuPrincipal();
     menuCliente();
+    menuFuncionario();
     telaSobre();
     return 0;
 }
@@ -52,7 +54,6 @@ void menuPrincipal(void) {
 	printf("///           2. Módulo Funcionário                                       ///\n");
 	printf("///           3. Módulo Gerente                                           ///\n");
 	printf("///           4. Sobre                                                    ///\n");
-	printf("///           0. Encerrar o programa                                      ///\n");
 	printf("///                                                                       ///\n");
 	printf("///           Escolha a opção desejada:                                   ///\n");
 	printf("///                                                                       ///\n");
@@ -64,42 +65,8 @@ void menuPrincipal(void) {
 }
 
 
-void telaSobre(void) {
-    system("clear");
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///             Universidade Federal do Rio Grande do Norte               ///\n");
-  	printf("///                 Centro de Ensino Superior do Seridó                   ///\n");
-  	printf("///               Departamento de Computação e Tecnologia                 ///\n");
-  	printf("///                  Disciplina DCT1106 -- Programação                    ///\n");
-  	printf("///            Projeto Sistema de Controle de Estacionamentos             ///\n");
-  	printf("///              = == ==       PARKINGMOD        = == ==                  ///\n");
-  	printf("///              Developed by  @rauan-meirelles - Jan, 2021               ///\n");
-  	printf("///                               (o ! o)                                 ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-  	printf("///                                                                       ///\n");
-	printf("///   == == ParkingMod: Um Sistema de Controle de Estacionamentos == ==   ///\n");
-  	printf("///                                                                       ///\n");
-  	printf("///    Programa utilizado com a intenção de facilitar a admnistração de   ///\n");
-  	printf("///   estacionamentos, para fins de organização e de admnistração. O      ///\n");
-  	printf("///   programa contém os principais módulos e funcionalidades úteis que   ///\n");
-  	printf("///   serão usados para tornar o estacionamneto mais viável e mais bem    ///\n");
-  	printf("///   avaliado por seus ultilizadores. Como: Vagas disponíveis e          ///\n");
-  	printf("///   Indisponíveis, Preços por hora das vagas , entre outros, como       ///\n");
-  	printf("///   o Lucro final diário, Avaliação do cliente e Sugestões de melhoras. ///\n");
-  	printf("///                                                                       ///\n");
-  	printf("/////////////////////////////////////////////////////////////////////////////\n");
-  	printf("///                                                                       ///\n");
-  	printf("///    Em desenvolvimento pelo aluno de Sistemas da Informação - UFRN     ///\n");
-  	printf("///                 Rauan Meirelles Dantas de Araújo                      ///\n");
-  	printf("///                                                                       ///\n");
-  	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-	getchar();
-}
+
+
 
 
 void menuCliente(void) {
@@ -111,7 +78,7 @@ void menuCliente(void) {
 	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
 	printf("///              (o ! o) == ==  PARKINGMOD   = == == (o ! o)              ///\n");
 	printf("///                Sistema de Controle de Estacionamentos                 ///\n");
-  	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
 	printf("///          ===================================================          ///\n");
 	printf("///              Developed by  @rauan-meirelles - Jan, 2021               ///\n");
 	printf("///                                                                       ///\n");
@@ -123,8 +90,8 @@ void menuCliente(void) {
 	printf("///                                                                       ///\n");
 	printf("///           1. Alocar uma Vaga                                          ///\n");
 	printf("///           2. Tipos de Vaga                                            ///\n");
-	printf("///           3. Preço de Vaga                                            ///\n");
-	printf("///           4. Desalocar Vaga                                           ///\n");
+	printf("///           3. Tabela de Preços e Vagas disponiveis                     ///\n");
+	printf("///           3. Desalocar Vaga                                           ///\n");
 	printf("///           0. Voltar ao menu anterior                                  ///\n");
 	printf("///                                                                       ///\n");
 	printf("///           Escolha a opção desejada:                                   ///\n");
@@ -133,5 +100,82 @@ void menuCliente(void) {
 	printf("/////////////////////////////////////////////////////////////////////////////\n");
 	printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+
+
+
+
+
+void menuFuncionario(void) {
+    system("clear");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///              (o ! o) == ==  PARKINGMOD   = == == (o ! o)              ///\n");
+	printf("///                Sistema de Controle de Estacionamentos                 ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///              Developed by  @rauan-meirelles - Jan, 2021               ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           (o ! o) = = = Menu Funcionário  = = =  (o ! o)              ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           1. Vagas Disponíveis/Ocupadas                               ///\n");
+	printf("///           2. Tabela de Preços                                         ///\n");
+	printf("///           3. Alterar preço das vagas                                  ///\n");
+	printf("///           0. Voltar ao menu anterior                                  ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           Escolha a opção desejada:                                   ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+}
+
+
+
+
+void telaSobre(void) {
+    system("clear");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///             Universidade Federal do Rio Grande do Norte               ///\n");
+ 	 printf("///                 Centro de Ensino Superior do Seridó                   ///\n");
+ 	 printf("///               Departamento de Computação e Tecnologia                 ///\n");
+  	printf("///                  Disciplina DCT1106 -- Programação                    ///\n");
+  	printf("///            Projeto Sistema de Controle de Estacionamentos             ///\n");
+  	printf("///              = == ==       PARKINGMOD        = == ==                  ///\n");
+	printf("///              Developed by  @rauan-meirelles - Jan, 2021               ///\n");
+	printf("///                               (o ! o)                                 ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///   == == ParkingMod: Um Sistema de Controle de Estacionamentos == ==   ///\n");
+	printf("///                                                                       ///\n");
+	printf("///    Programa utilizado com a intenção de facilitar a admnistração de   ///\n");
+	printf("///   estacionamentos, para fins de organização e de admnistração. O      ///\n");
+	printf("///   programa contém os principais módulos e funcionalidades úteis que   ///\n");
+	printf("///   serão usados para tornar o estacionamneto mais viável e mais bem    ///\n");
+	printf("///   avaliado por seus ultilizadores. Como: Vagas disponíveis e          ///\n");
+	printf("///   Indisponíveis, Preços por hora das vagas , entre outros, como       ///\n");
+	printf("///   o Lucro final diário.                                               ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///    Em desenvolvimento pelo aluno de Sistemas da Informação - UFRN     ///\n");
+	printf("///                 Rauan Meirelles Dantas de Araújo                      ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 	getchar();
 }
