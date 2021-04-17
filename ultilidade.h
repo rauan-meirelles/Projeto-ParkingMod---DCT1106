883c8b31ae84fc14341c7b50e8629706d9031447
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2021-04-17 11:32:21 -0300

    Create ultilidade.h

---
+++ ultilidade.h
@@ -0,0 +1,18 @@
+////////////////////////////////////////
+////       Módulo Ultilidades       ////
+////////////////////////////////////////
+
+#define True 1
+#define False 0
+
+void delay(int);
+void limpaTela(void);
+int ehDigito(char);
+int ehLetra(char);
+int ehBissexto(int);
+int ehData(int, int, int);
+
+int validarNome(char*);
+int validarData(char*);
+int validarCPF(char);
+int validarPlaca(char*);
