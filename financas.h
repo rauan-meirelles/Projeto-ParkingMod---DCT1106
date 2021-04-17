ab52999b6607344c477ff5af9bf36c79e76b6e1d
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2021-04-17 11:29:22 -0300

    Módulo Finanças

---
+++ financas.h
@@ -0,0 +1,8 @@
+////////////////////////////////////////
+////         Módulo Finanças        ////
+////////////////////////////////////////
+
+void menuFinancas(void);
+void telaCadastroPre(void);
+void telaAtualizarPre(void);
+void telaLucroDiario(void);
