daa7aa1c43332023594082789c63e87f8128e856
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2021-04-17 11:30:44 -0300

    Módulo Funcionário

---
+++ funcionario.h
@@ -0,0 +1,8 @@
+////////////////////////////////////////
+////       Módulo Funcionario       ////
+////////////////////////////////////////
+
+void menuFuncionario(void);
+void telaCadastroFun(void);
+void telaAtualizarFun(void);
+void telaExcluirFun(void);
