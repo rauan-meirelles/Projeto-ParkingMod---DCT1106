31b578098eb77f83f622c5677e64e20b02c593b0
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2021-04-19 18:41:21 -0300

    Update financas.h

--- financas.h
+++ financas.h
@@ -1,8 +1,32 @@
 ////////////////////////////////////////
-////         Módulo Finanças        ////
+////         Módulo Gerençia        ////
 ////////////////////////////////////////
 
-void menuFinancas(void);
-void telaCadastroPre(void);
-void telaAtualizarPre(void);
-void telaLucroDiario(void);
+typedef struct gerente Gerente;
+
+
+struct gerente {
+  char placa[12];
+  char nome[51];
+  char nasc[11];
+  char cpf[12];
+  int status;
+};
+
+
+void moduloGerente(void);
+char menuGerente(void);
+Gerente* telaCadastrarGerente(void);
+void* telaPesquisarGerente(void);
+void* telaAtualizarGerente(void);
+void* telaExcluirGerente(void);
+void telaErroGerente(void);
+void cadastrarGerente(void);
+void pesquisarGerente(void);
+void atualizarGerente(void);
+void excluirGerente(void);
+void gravarGerente(Gerente*);
+void exibirGerente(Gerente*);
+Gerente* buscarGerente(char*);
+void exibirGerente(Gerente*);
+void regravarGerente(Gerente*);
