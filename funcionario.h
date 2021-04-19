78c3c5c14963efb38fbb53201428a78d4d43b7c6
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2021-04-19 18:39:44 -0300

    Update funcionario.h

--- funcionario.h
+++ funcionario.h
@@ -2,7 +2,31 @@
 ////       Módulo Funcionario       ////
 ////////////////////////////////////////
 
-void menuFuncionario(void);
-void telaCadastroFun(void);
-void telaAtualizarFun(void);
-void telaExcluirFun(void);
+typedef struct funcionario Funcionario;
+
+
+struct funcionario {
+  char placa[12];
+  char nome[51];
+  char nasc[11];
+  char cpf[12];
+  int status;
+};
+
+
+void moduloFuncionario(void);
+char menuFuncionario(void);
+Funcionario* telaCadastrarFuncionario(void);
+void* telaPesquisarFuncionario(void);
+void* telaAtualizarFuncionario(void);
+void* telaExcluirFuncionario(void);
+void telaErroFuncionario(void);
+void cadastrarFuncionario(void);
+void pesquisarFuncionario(void);
+void atualizarFuncionario(void);
+void excluirFuncionario(void);
+void gravarFuncionario(Funcionario*);
+void exibirFuncionario(Funcionario*);
+Funcionario* buscarFuncionario(char*);
+void exibirFuncionario(Funcionario*);
+void regravarFuncionario(Funcionario*);
