4e4a306ff26ff1122d8959a2aa55b30fdbbbbe9f
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2021-05-05 16:27:32 -0300

    Create estaciona.h

---
+++ estaciona.h
@@ -0,0 +1,52 @@
+////////////////////////////////////////
+////         Módulo Estaciona       ////
+////////////////////////////////////////
+
+typedef struct estaciona Estaciona;
+
+struct estaciona {
+  int dia;
+  int mes;
+  int ano;
+  char placa[8];
+  int status;
+}; 
+
+///Entrada:
+///ler placa
+///ler data
+///status = 1;
+///gravar
+
+///Saida:
+///ler placa
+///status = 0;
+///regravar (editar)
+
+void moduloEstaciona(void);
+char menuEstaciona(void);
+
+Estaciona* telaEstacionarVei(void);
+void cadastrarEst(void);
+void gravarEst(Estaciona*);
+
+char* telaRetirarVei(void);
+void retirarVei(void);
+Estaciona* buscarEst(char*);
+
+void telaErro(void);
+
+
+
+// char* telaPesquisarEst(void);
+// char* telaAtualizarVei(void);
+// void telaErroEstaciona(void);
+// void cadastrarEst(void);
+// void pesquisarVei(void);
+// void atualizarVei(void);
+// void retirarEst(void);
+
+// void exibirVei(Cliente*);
+
+// void exibirEst(Cliente*);
+
