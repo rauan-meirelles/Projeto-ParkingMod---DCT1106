5e440a3ea2b7e747ae7e03fe054d9159beec327e
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2021-05-05 16:26:12 -0300

    Create relatorio.h

---
+++ relatorio.h
@@ -0,0 +1,11 @@
+////////////////////////////////////////
+////         Módulo Relatório       ////
+////////////////////////////////////////
+
+
+void moduloRelatorio(void);
+char menuRelatorio(void);
+
+void relatorioCliente(Cliente*);
+///void relatorioFuncionario(Cliente*);
+///void relatorioGerente(Cliente*);
