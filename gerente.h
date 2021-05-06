decf1e7650242764088fe3b6f986395934ca08fe
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2021-05-06 03:41:21 -0300

    Update gerente.h

--- gerente.h
+++ gerente.h
@@ -17,17 +17,21 @@ struct gerente {
 
 void moduloGerente(void);
 char menuGerente(void);
+
 Gerente* telaCadastrarGerente(void);
 void* telaPesquisarGerente(void);
 void* telaAtualizarGerente(void);
 void* telaExcluirGerente(void);
-void telaErroGerente(void);
+
 void cadastrarGerente(void);
 void pesquisarGerente(void);
 void atualizarGerente(void);
 void excluirGerente(void);
+
 void gravarGerente(Gerente*);
 void exibirGerente(Gerente*);
 Gerente* buscarGerente(char*);
 void exibirGerente(Gerente*);
 void regravarGerente(Gerente*);
+
+void telaErroGerente(void);
